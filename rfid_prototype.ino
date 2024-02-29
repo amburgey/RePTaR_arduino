cfa96584f6d11eaffaf8b8b62adec7c57b7ead5c
Aadu Prakash <aadu@uw.edu>
2024-02-29 14:16:44 -0800

    Added comments to code and documentation folder

--- rfid_prototype.ino
+++ rfid_prototype.ino
@@ -48,7 +48,10 @@ void setup()
 {
     String output;
     bool file_exists = false;
-    
+ 
+    /* Begin Serial comms w/ computer and both 
+       RFID boards */
+   
     Serial.begin(115200);
     rfid_1_serial.begin(9600);
     rfid_2_serial.begin(9600);
@@ -56,23 +59,27 @@ void setup()
       
     if (! rtc.begin()) 
     {
-        //Serial.println("Couldn't find RTC");
+        Serial.println("Couldn't find RTC");
         abort();
     }
 
-    //if(1)
+    /* Make sure RTC is initialized with proper date and time. Datetime is grabbed from 
+       computer uploading code to the Arduino */
     if (! rtc.initialized() || rtc.lostPower()) 
     {
         Serial.println("RTC is NOT initialized, let's set the time!");
         rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     }
     
+    /* Initialize SD card with SD CS Pin */
     if (!SD.begin(SD_CHIP_SELECT))
     {
-        //Serial.println("Card init. failed!");
+        Serial.println("Card init. failed!");
         abort();
     }
 
+    /* We use non-volatile memory to store the device address of the RePTaR unit. If this is
+       uninitialized, then we write the device ID to the memory address */
     device_id = EEPROM.read(DEVICE_ID_ADDR);
 
     if (device_id == 0xFF)
@@ -80,6 +87,7 @@ void setup()
         EEPROM.write(DEVICE_ID_ADDR, DEVICE_ID);
     }
 
+    /* Start a log based on current datetime */
     DateTime now = rtc.now();
     logFilename = String(now.year()) + String(now.month()) + String(now.day()) + ".csv";
     
@@ -90,9 +98,10 @@ void setup()
     logFile = SD.open(logFilename, FILE_WRITE);
     if (logFile)
     {
+        /* If file does not exist, then create file */
         if (!file_exists)
         {
-            //Serial.println("New File created");
+            Serial.println("New File created");
             output = "timestamp,device_num,antenna_num,rfid_id";
             logFile.println(output);
             logFile.flush();
@@ -101,10 +110,11 @@ void setup()
     }
     else
     {
-        //Serial.println("File not created");
+        Serial.println("File not created");
         abort();
     }
 
+    /* Start the RTC */
     rtc.start();
 }
 
@@ -114,8 +124,10 @@ void loop()
     static unsigned long detection_time_1 = 0;
     static unsigned long detection_time_2 = 0;
     
+    /* Check if enough time has elapsed since last read of RFID 1 */
     if (millis () - detection_time_1 >= DETECTION_SLEEP_TIME || detection_time_1 == 0)
     {
+        /* Turn on the RF field, and see if any IDs were captured */
         start_rfid_capture(&rfid_1_serial);
         delay(100);
         returned_id = stop_rfid_capture(&rfid_1_serial, 1);
@@ -128,8 +140,10 @@ void loop()
         delay (10);
     }
 
+    /* Check if enough time has elapsed since last read of RFID 2 */
     if (millis() - detection_time_2 >= DETECTION_SLEEP_TIME || detection_time_2 == 0)
     {
+        /* Turn on the RF field, and see if any IDs were captured */
         start_rfid_capture(&rfid_2_serial);
         delay(100);
         returned_id = stop_rfid_capture(&rfid_2_serial, 2);
@@ -143,6 +157,8 @@ void loop()
     }
 }
 
+
+/* Flush all characters to be read in the RFID Serial stream */
 void flush_buffer(SoftwareSerial *rfid_serial)
 {
     /* Flush all characters in buffer */
@@ -153,12 +169,19 @@ void flush_buffer(SoftwareSerial *rfid_serial)
     }
 }
 
+/* Initializes RFID chips on both boards. Grabs the frequency of the antenna and prints to 
+   the screen. This is useful for debugging purposes as you set up the RePTaR unit.
+   Refer to the RFID datasheet for more information on the serial commands  */
 void init_rfid()
 {
     float freq_1 = 0.0;
     float freq_2 = 0.0;
+    
+    /* Use FDX-B standard */
     rfid_1_serial.print("SD2\r");
+    /* Deactivate Reader */
     rfid_1_serial.print("SRD\r");
+    /* Read Standard Data */
     rfid_1_serial.print("RSD\r");
     delay(300);
 
@@ -168,8 +191,11 @@ void init_rfid()
     freq_1 = get_rfid_freq(&rfid_1_serial);
     Serial.println("Antenna 1 Frequency: " + String(freq_1) + " kHz");
     
+    /* Use FDX-B standard */
     rfid_2_serial.print("SD2\r");
+    /* Deactivate Reader */
     rfid_2_serial.print("SRD\r");
+    /* Read Standard Data */
     rfid_2_serial.print("RSD\r");
     delay(300);
 
@@ -179,23 +205,28 @@ void init_rfid()
     Serial.println("Antenna 2 Frequency: " + String(freq_2) + " kHz");
 }
 
+/* Return the antenna frequency attached to the specified RFID board */
 float get_rfid_freq(SoftwareSerial *rfid_serial)
 {
     float retv = 0.0;
+    /* Measure Operating Frequency */
     rfid_serial->print("MOF\r");
     
     delay(1400);
     rfid_serial->listen();
     if(rfid_serial->available())
     {
+	/* Get the float that is returned by the RFID board */
         retv = rfid_serial->parseFloat();
     }
     
     return retv;
 }
 
+/* Turn on the RF field and begin a capture */
 void start_rfid_capture(SoftwareSerial *rfid_serial)
 {
+    /* Set Reader Active */
     rfid_serial->print("SRA\r");
     flush_buffer(rfid_serial);
 }
@@ -207,14 +238,16 @@ uint32_t stop_rfid_capture(SoftwareSerial *rfid_serial, int antenna_num)
     String output;
     
     uint32_t captured_id = check_rfid_id(rfid_serial);
+    /* If a valid id has been captured, then log it */
     if(captured_id)
     {
-        //Serial.println("ID: " + String(captured_id));
+        Serial.println("ID: " + String(captured_id));
 
         now = rtc.now();
         epochString = String(now.unixtime());
         output = epochString + "," + String(device_id) + "," + String(antenna_num) + "," + String(captured_id);
         
+        /* Open the logfile and write the device id, antenna id, RFID number, and datetime in a single line */
         logFile = SD.open(logFilename, FILE_WRITE);
         if (logFile)
         {       
@@ -227,12 +260,17 @@ uint32_t stop_rfid_capture(SoftwareSerial *rfid_serial, int antenna_num)
             Serial.println("What happened?");
         }    
     }
+    /* Disable the field once complete */
     rfid_serial->print("SRD\r");
     flush_buffer(rfid_serial);
 
     return captured_id;
 }
 
+
+/* FDX-B PIT Tag IDs are delimited with a '_'. We read until we find a '_' character and then
+   parse the int that follows. We went ahead and discarded the first 3 digits after the '_' 
+   character */ 
 uint32_t check_rfid_id(SoftwareSerial *rfid_serial)
 {
     uint32_t retv = 0;
